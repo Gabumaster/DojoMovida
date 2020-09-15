9c47d9fa14dfec741f3fbc4900287538d366c768
Gabriel Passos Da Silva <GSIL65@NEXTEL.COM.BR>
2020-09-15 16:00:17 -0300

    Adicao_Lib_Faker

--- features/step_definitions/cadastro_steps.rb
+++ features/step_definitions/cadastro_steps.rb
@@ -5,21 +5,30 @@ Dado("que eu esteja na pagina de cadastro do site") do
 end
 
 Quando("realizo a criação da conta") do
+  Faker::Config.locale = 'pt-BR'
+  cpf = Faker::CPF.numeric
+  nome = Faker::Games::Zelda.location
+  telnumero = Faker::PhoneNumber.phone_number
+  datanasc = Faker::Date.birthday(min_age: 35, max_age: 50).strftime("%d/%m/%Y")
+  email = Faker::Internet.free_email
+  confemail = email
+
+
   find('div[class="name-user-menu"]').click
   find('a[class="user-menu-link-out"]').click
-  find('input[class="form-control cpf"]').fill_in :with => '19535362801'
-  find('input[id="nome"]').fill_in :with => 'Gabu'
+  find('input[class="form-control cpf"]').fill_in :with => cpf
+  find('input[id="nome"]').fill_in :with => nome
   find('label[for="feminino"]').click
-  find('input[id="data_nasc"]').fill_in :with => '04071985'
-  find('input[id="telefone"]').fill_in :with => '1112341234'
-  find('input[id="celular"]').fill_in :with => '11123412341'
-  find(:xpath, '(//input[@id="email"])[1]').fill_in :with => 'gabriel.silva6@nextel.com.br'
-  find('input[id="email_conf"]').fill_in :with => 'gabriel.silva6@nextel.com.br'
+  find('input[id="data_nasc"]').fill_in :with => datanasc
+  find('input[id="telefone"]').fill_in :with => telnumero
+  find('input[id="celular"]').fill_in :with => '11923412341'
+  find(:xpath, '(//input[@id="email"])[1]').fill_in :with => email
+  find('input[id="email_conf"]').fill_in :with => confemail
   find('input[id="cep"]').fill_in :with => '01234567'
-  find('input[id="logradouro"]').fill_in :with => 'Avenida Cupecê'
-  find('input[id="numero"]').fill_in :with => '6062'
-  find('input[id="complemento"]').fill_in :with => 'Bl8 Ap 63'
-  find('input[id="bairro"]').fill_in :with => 'Jardim Miriam'
+  find('input[id="logradouro"]').fill_in :with => 'Avenida Copopencio'
+  find('input[id="numero"]').fill_in :with => '413'
+  find('input[id="complemento"]').fill_in :with => 'Ap24'
+  find('input[id="bairro"]').fill_in :with => 'Jardim Sebostilho'
   find('select[id="Pais"]').click
   find('option[value="1"]').click
   find('select[id="uf_sel"]').click
@@ -28,9 +37,11 @@ Quando("realizo a criação da conta") do
   find('option[value="3537909"]').click
   find('input[id="senha_cadastro"]').fill_in :with => '123456'
   find('input[id="senha_conf"]').fill_in :with => '123456'
-  binding.pry
 end
 
 Então("devo logar no site com os dados criados") do
-  pending # Write code here that turns the phrase above into concrete actions
-end
+  find('button[id="btnEnviaDados"]').click
+  sleep 15
+  find('div[class="alert alert-warning"]')
+  puts 'Cadastro efetuado com sucesso.'
+ end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,6 +1,8 @@
 require 'capybara'
 require 'capybara/cucumber'
 require 'pry'
+require 'faker'
+require 'cpf_faker'
 
 Capybara.configure do |config|
     config.default_driver = :selenium
